06fb441affe50e9d6bcdb681be0749f8acf4417d
mburugudestinygakenga <mburugugakenga@gmail.com>
2024-12-15 19:21:34 +0300

    starter

--- models/article.py
+++ models/article.py
@@ -8,3 +8,34 @@ class Article:
 
     def __repr__(self):
         return f'<Article {self.title}>'
+    
+    @property
+    def id (self):
+        return self._id
+    
+    @id.setter
+    def id (self, id):
+        self._id = id 
+
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, value):
+        if not isinstance(value, str) or len(value) < 5 or len(value) > 50:
+            raise ValueError("Title must be a string between 5 and 50 characters")
+        self._title = value
+
+    @property
+    def content(self):
+        return self._content
+    
+    @content.setter
+    def content(self, content):
+        if isinstance(content, str) and len(content):
+            self._content = content
+    
+    @property
+    def magazine(self):
+      return self._magazine
